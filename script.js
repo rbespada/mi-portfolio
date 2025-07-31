33eafe6e29a21921ac34da37774cfc6995b3b84d
rbespada <rbuendiaespada@gmail.com>
2025-07-31 13:02:23 +0200

    Añadida seccion 'Red empresarial simulada' // añadido efecto hover de sombra y desplazamiento y cursor pointer // mejoras en el diseño de presentacion con avatar y texto al lateral // Corregidos problemas con boton hamburguesa al reducir la pantalla (Problemas en CSS tenia reglas fuera de de una media query) // Revisado diseño y empezando a adaptarlo al prototipo de Figma. Suficiente por hoy :).

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
 // Esperamos a que todo el contenido esté cargado
 
 document.addEventListener('DOMContentLoaded', () => {
+
+
+
+
+// ----- Cookies -----
+
   const popup = document.getElementById('cookies-popup');
   const aceptar = document.getElementById('aceptar-cookies');
   const rechazar = document.getElementById('rechazar-cookies');
 
-
-
- // Verificamos si ya se ha aceptado o rechazado previamente
-  
- if (!localStorage.getItem('cookiesAceptadas')) {
+  if (!localStorage.getItem('cookiesAceptadas')) {
     popup.style.display = 'block';
   }
 
-
-
-// Al hacer clic en "Aceptar"
-
-aceptar.addEventListener('click', () => {
+  aceptar.addEventListener('click', () => {
     localStorage.setItem('cookiesAceptadas', 'true');
     popup.style.display = 'none';
   });
 
-
-
-// Al hacer clic en "Rechazar"
-
-rechazar.addEventListener('click', () => {
+  rechazar.addEventListener('click', () => {
     localStorage.setItem('cookiesAceptadas', 'false');
     popup.style.display = 'none';
   });
+
+
+
+
+
+// ----- Botón hamburguesa -----
+
+  const menuToggle = document.getElementById('menu-toggle');
+  const nav = document.getElementById('main-nav');
+
+  menuToggle.addEventListener('click', () => {
+    
+    console.log('Click detectado en botón hamburguesa'); //para pruebas (estoy teniendo problemas con ocultar el menu en movil o pantalla pequeña)
+
+    nav.classList.toggle('activo');
+  });
 });
 
-
-
-// Activar botón hamburguesa 
-
-const menuToggle = document.getElementById('menu-toggle');
-const nav = document.getElementById('main-nav');
-
-menuToggle.addEventListener('click', () => {
-  nav.classList.toggle('activo');
-});
